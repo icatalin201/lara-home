7cf2d11f20c845f9fe822f76e29ecfa2d1450905
Catalin Matache <icatalin201@gmail.com>
2021-02-11 23:44:48 +0200

    add application entry point

---
+++ infrastructure/spring-boot-application/src/main/java/LaraHomeApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** lara Created by Catalin on 2/11/2021 */
+@SpringBootApplication
+public class LaraHomeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LaraHomeApplication.class, args);
+  }
+}
